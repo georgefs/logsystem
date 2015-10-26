134ce06b0cbbb76aa32a2e9f3e39ec8ec5850ce5
george <goblin_george@gmail.com>
2015-10-26 09:07:56 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2015 lizongzhe 
+#
+# Distributed under terms of the MIT license.
+
+import sys
+import yaml
+import os
+conf_file = sys.argv[1]
+hostname = os.popen('hostname').read()
+conf = yaml.load(open(conf_file).read().format(env=os.environ, hostname=hostname))
+env = conf['env']
+export_env = "\n".join(["export {}={};".format(k, v) for k, v in env.items()])
+del conf['env']
+for key, info in conf.items():
+    path = info['path']
+    template = info['template']
+#    with open(path, 'w+') as out:
+#        out.write(template.format(env=env, export_env=export_env))
+    print template.format(env=env, export_env=export_env)
+
+
